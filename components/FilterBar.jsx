f28f4c974b9adb5bb4c589b6a99fee53d4ec2416
habibhamdoun <100615843+habibhamdoun@users.noreply.github.com>
2023-08-09 04:23:58 +0300

    added : categories creating categories

---
+++ components/FilterBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Pressable, ScrollView, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+const FilterBar = ({
+  categories,
+  activeCategorie,
+  activateCategorie,
+  setCategorieInput,
+}) => {
+  return (
+    <View style={tw`h-16`}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={tw`flex flex-row items-start`}>
+          {categories.map((categorie) => {
+            return (
+              <Pressable
+                key={categorie}
+                onPress={() => activateCategorie(categorie)}
+                style={
+                  activeCategorie == categorie
+                    ? tw`border-2 border-gray-300 bg-green-500 mx-1 p-2 rounded-lg`
+                    : tw`border-2 border-gray-300 bg-white mx-1 p-2 rounded-lg`
+                }
+              >
+                <Text
+                  style={
+                    activeCategorie == categorie
+                      ? tw` text-white`
+                      : tw` text-black`
+                  }
+                >
+                  {categorie}
+                </Text>
+              </Pressable>
+            );
+          })}
+          <Pressable
+            style={tw`border-2 border-gray-300 bg-white mx-1 p-2 rounded-lg`}
+            onPress={() => setCategorieInput(true)}
+          >
+            <Text>+</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default FilterBar;
--- pages/Hero.jsx
+++ pages/Hero.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Pressable, Text, TextInput, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import Logo from '../components/Logo';
 import tw from 'tailwind-react-native-classnames';
 import Task from '../components/Task';
+import FilterBar from '../components/FilterBar';
 
 const Hero = () => {
   const [list, setList] = useState([
@@ -11,30 +13,35 @@ const Hero = () => {
       content: 'this is content 1',
       completed: false,
       id: 0,
+      categorie: 'Personal',
     },
     {
       title: 'element2',
       content: 'this is content 2',
       completed: false,
       id: 1,
+      categorie: 'Important',
     },
     {
       title: 'element3',
       content: 'this is content 3',
       completed: false,
       id: 2,
+      categorie: 'To-do List',
     },
     {
       title: 'element4',
       content: 'this is content 4',
       completed: false,
       id: 3,
+      categorie: 'Lecture Notes',
     },
     {
       title: 'element5',
       content: 'this is content ',
       completed: false,
       id: 4,
+      categorie: 'Important',
     },
   ]);
   const [inputStatus, setInputStatus] = useState(false);
@@ -45,6 +52,23 @@ const Hero = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [errorMsg, setErrorMsg] = useState(false);
   const [editingTask, setEditingTask] = useState(null);
+  const [categorieInput, setCategorieInput] = useState(false);
+  const [selectedCategorie, setSelectedCategorie] = useState('');
+  const [newCategorie, setNewCategorie] = useState('');
+  const [categorieExistMsg, setCategorieExistMsg] = useState(false);
+  const [categorieErrorMsg, setCategorieErrorMsg] = useState(false);
+  const [categories, setCategories] = useState([
+    'All',
+    'Personal',
+    'Important',
+    'Lecture Notes',
+    'To-do List',
+    'Uncompleted',
+  ]);
+  const [activeCategorie, setActiveCategorie] = useState('All');
+  function activateCategorie(cat) {
+    setActiveCategorie(cat);
+  }
 
   function editTask(element) {
     setInputStatus(true);
@@ -55,9 +79,7 @@ const Hero = () => {
     setContent(element.content);
     setEditingTask(element);
   }
-  function addElement() {
-    setInputStatus((prev) => !prev);
-  }
+
   function setElement() {
     if (content != '') {
       let taskId = list[list.length - 1].id + 1;
@@ -68,12 +90,14 @@ const Hero = () => {
           content: content,
           completed: false,
           id: taskId,
+          categorie: selectedCategorie,
         },
       ]);
       setTitle('');
       setContent('');
       setInputStatus(false);
       setErrorMsg(false);
+      setSelectedCategorie('');
     } else setErrorMsg(true);
   }
   function taskCompleted(id) {
@@ -90,15 +114,84 @@ const Hero = () => {
     setList(updatedTasks);
   }
   // TODO: add random colors to boxes
-  //TODO: on press go to each task and edit
   //TODO: add attributes such as important / personal ... and then filter with each one
   //TODO: add local storage
   //TODO: add types such as list or bullet points
+  //TODO: allow user to add categories
+  // TODO: allow users to delete categories
   return (
     <View style={tw`flex justify-start items-center pt-14 flex-1 relative`}>
+      {categorieInput && (
+        <View
+          style={tw`absolute bg-white z-10 h-full w-full flex items-center justify-center`}
+        >
+          <View
+            style={tw` bg-white shadow-md h-96 rounded-xl flex items-center justify-center`}
+          >
+            <Pressable
+              onPress={() => {
+                setCategorieErrorMsg(false);
+                setCategorieExistMsg(false);
+                setCategorieInput(false);
+                setNewCategorie('');
+              }}
+            >
+              <Text
+                style={tw`flex items-end min-w-full mr-5 text-right text-2xl text-green-600 font-bold`}
+              >
+                x
+              </Text>
+            </Pressable>
+            <TextInput
+              style={tw`rounded-xl h-10 border-green-600 border-2 m-2 p-2 w-48`}
+              placeholder='Insert categorie...'
+              placeholderTextColor={'#000000'}
+              onChangeText={(newText) => setNewCategorie(newText)}
+              value={newCategorie}
+            />
+            {categorieExistMsg && (
+              <Text style={tw`text-red-600 p-2`}>
+                Categorie already existant.
+              </Text>
+            )}
+            {categorieErrorMsg && (
+              <Text style={tw`text-red-600 p-2`}>
+                Please fill in a Categorie name.
+              </Text>
+            )}
+            <Pressable
+              onPress={() => {
+                if (categories.indexOf(newCategorie) != -1) {
+                  setCategorieExistMsg(true);
+                  return;
+                } else if (newCategorie == '') {
+                  setCategorieErrorMsg(true);
+                } else {
+                  const updatedCategories = [...categories, newCategorie];
+                  setCategories(updatedCategories);
+                  setCategorieInput(false);
+                  setCategorieErrorMsg(false);
+                  setCategorieExistMsg(false);
+                  setNewCategorie('');
+                }
+              }}
+            >
+              <Text
+                style={tw`text-white bg-green-600 rounded-lg p-2 flex items-center justify-center text-center`}
+              >
+                Submit Categorie
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      )}
       {inputStatus && (
-        <View style={tw`absolute bg-white z-10 h-full w-full`}>
-          <View style={tw`top-1/3 bg-white shadow-md p-16 rounded-xl `}>
+        <View
+          style={tw`absolute bg-white z-10 h-full w-full flex items-center justify-center`}
+        >
+          <View
+            style={tw` bg-white shadow-md h-96 rounded-xl flex items-center justify-center`}
+          >
             <Pressable
               onPress={() => {
                 setInputStatus(false);
@@ -131,6 +224,26 @@ const Hero = () => {
               onChangeText={(newText) => setContent(newText)}
               value={content}
             />
+            <View
+              style={tw`border-2 border-gray-300 bg-green-500 rounded-lg m-2`}
+            >
+              <Picker
+                style={tw`w-28 text-white`}
+                selectedValue={selectedCategorie}
+                onValueChange={(itemValue) => setSelectedCategorie(itemValue)}
+              >
+                {categories.map((categorie) => {
+                  return (
+                    <Picker.Item
+                      key={categorie}
+                      label={categorie}
+                      value={categorie}
+                    />
+                  );
+                })}
+              </Picker>
+            </View>
+
             <View style={tw`flex flex-row justify-around`}>
               {isEditing ? (
                 <Pressable
@@ -169,7 +282,7 @@ const Hero = () => {
                   }
                 >
                   <Text
-                    style={tw`text-white bg-green-600 rounded-lg w-16 p-2 flex items-center justify-center`}
+                    style={tw`text-white bg-green-500 rounded-lg  p-2 flex items-center justify-center border-2 border-gray-300`}
                   >
                     Submit Task!
                   </Text>
@@ -182,17 +295,48 @@ const Hero = () => {
       <View style={tw`pb-7`}>
         <Logo />
       </View>
+      <View>
+        <FilterBar
+          categories={categories}
+          activeCategorie={activeCategorie}
+          activateCategorie={activateCategorie}
+          setCategorieInput={setCategorieInput}
+        />
+      </View>
       <View style={tw`flex flex-row min-w-full flex-wrap justify-around`}>
         {list.map((element) => {
-          return (
-            <Task
-              key={element.title + element.content}
-              element={element}
-              taskCompleted={taskCompleted}
-              taskUncompleted={taskUncompleted}
-              editTask={editTask}
-            />
-          );
+          if (element.categorie == activeCategorie) {
+            return (
+              <Task
+                key={element.title + element.content}
+                element={element}
+                taskCompleted={taskCompleted}
+                taskUncompleted={taskUncompleted}
+                editTask={editTask}
+              />
+            );
+          } else if (activeCategorie == 'All') {
+            return (
+              <Task
+                key={element.title + element.content}
+                element={element}
+                taskCompleted={taskCompleted}
+                taskUncompleted={taskUncompleted}
+                editTask={editTask}
+              />
+            );
+          } else if (activeCategorie == 'Uncompleted') {
+            if (element.completed == false)
+              return (
+                <Task
+                  key={element.title + element.content}
+                  element={element}
+                  taskCompleted={taskCompleted}
+                  taskUncompleted={taskUncompleted}
+                  editTask={editTask}
+                />
+              );
+          }
         })}
       </View>
       <View style={tw`flex items-end min-w-full mr-5`}>
@@ -201,7 +345,7 @@ const Hero = () => {
         >
           <Pressable
             onPress={() => {
-              addElement();
+              setInputStatus(true);
               setIsEditing(false);
             }}
           >
